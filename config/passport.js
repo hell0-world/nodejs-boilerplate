c348d9811c7c38eda5084b9d0ae44a16727969ed
hyojoon <pkpk5087@gmail.com>
2020-12-31 16:45:13 +0900

    [fix] - checkAuth middleware

--- config/passport.js
+++ config/passport.js
@@ -60,11 +60,7 @@ exports.checkAuth = (req, res, next) => {
   passport.authenticate("jwt", { session: false }, (err, payload, info) => {
     if (err) return next(err);
     if (!payload) return next(info);
-
-    User.findOne({ email: payload.email }, (err, user) => {
-      if (err) return next(err);
-      if (!user) return next("no matching user found");
-      res.status(200).send({ email: user.email });
-    });
+    req.user = payload;
+    next();
   })(req, res, next);
 };
