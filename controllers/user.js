c01197708837823451d7f712d80a53c42f1d095c
hyojoon <pkpk5087@gmail.com>
2021-01-11 14:29:52 +0900

    [feat] - add token expire time to password reset email

--- controllers/user.js
+++ controllers/user.js
@@ -283,7 +283,10 @@ exports.postForgot = (req, res, next) => {
       text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
         Please click on the following link, or paste this into your browser to complete the process:\n\n
         http://${req.headers.host}/reset/${token}\n\n
-        If you did not request this, please ignore this email and your password will remain unchanged.\n`
+        If you did not request this, please ignore this email and your password will remain unchanged.\n\n
+        This link expires on ${new Date(
+          user.passwordResetExpires
+        ).toString()}\n`
     };
     const mailSettings = {
       successfulType: "info",
